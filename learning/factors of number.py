93b9582c762314f3c5a1027c84f97409f69cc772
shakthipriya-r <160471059+shakthipriya-r@users.noreply.github.com>
2025-02-16 17:28:35 -0800

    Update factors of number.py

--- learning/factors of number.py
+++ learning/factors of number.py
@@ -11,3 +11,16 @@ for i in range(n):
     i=input(("Enter your task:"))
     a.append(i)
 print(a)
+
+#peak element
+
+a = [1, -1, 5, 0, -8]
+b = []
+c=a
+# Loop through each item in the list
+for i in c:  # Use a copy of 'a' to avoid modification issues
+    if i < 0:
+        b.append(i)  # Move negative numbers to list b
+        a.remove(i)  # Remove negative numbers from list a
+
+print(a + b)  # Print positive numbers first, then negative ones
